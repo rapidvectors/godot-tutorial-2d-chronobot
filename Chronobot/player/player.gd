b7dd7436430f7aa696780f1e6aa6357e07d47497
rapidvectors <rapidvectors@gmail.com>
2023-11-08 22:13:05 +0000

    Create a player hit flash visual shader.

--- Chronobot/player/player.gd
+++ Chronobot/player/player.gd
@@ -4,6 +4,7 @@ var bullet = preload("res://player/bullet.tscn")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var muzzle : Marker2D = $Muzzle
+@onready var hit_animation_player = $HitAnimationPlayer
 
 const GRAVITY = 1000
 @export var speed : int = 1000
@@ -119,4 +120,5 @@ func input_movement():
 func _on_hurtbox_body_entered(body : Node2D):
 	if body.is_in_group("Enemy"):
 		print("Enemy entered ", body.damage_amount)
+		hit_animation_player.play("hit")
 		HealthManager.decrease_health(body.damage_amount)
