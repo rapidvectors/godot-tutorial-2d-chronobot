c46312183ae090621c90c0b5927e8818c5c5e350
rapidvectors <rapidvectors@gmail.com>
2023-11-16 13:33:08 +0000

    Updated the camera script to add camera smoothing and fix jittering

--- Chronobot/player/player_camera.gd
+++ Chronobot/player/player_camera.gd
@@ -1,7 +1,25 @@
 extends Camera2D
 
+@export_category("Follow Character")
 @export var player : CharacterBody2D
+
+@export_category("Camera Smoothing")
+@export var smoothing_enabled : bool
+@export_range(1, 10) var smoothing_distance : int = 8
+
+var weight : float
+
+func _ready():
+	weight = float(11 - smoothing_distance) / 100 
+
 
 func _physics_process(delta):
 	if player != null:
-		global_position = player.global_position
+		var camera_position : Vector2
+		
+		if smoothing_enabled:
+			camera_position = lerp(global_position, player.global_position, weight)
+		else:
+			camera_position = player.global_position
+		
+		global_position = camera_position.floor()
