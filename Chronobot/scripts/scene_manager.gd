81ad04a6208e44bdd3d8618948ff46651aa5b734
rapidvectors <rapidvectors@gmail.com>
2023-11-17 23:02:56 +0000

    Created a shader VFX for the game loading screen with scene transitions

--- Chronobot/scripts/scene_manager.gd
+++ Chronobot/scripts/scene_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var scene_transition_screen = preload("res://ui/screen_transition/scene_transition_screen.tscn")
+
 var scenes : Dictionary = { "Level1": "res://levels/level_1.tscn",
 							"Level2": "res://levels/level_2.tscn" }
 
@@ -8,5 +10,8 @@ func transition_to_scene(level : String):
 	var scene_path : String = scenes.get(level)
 	
 	if scene_path != null:
-		await get_tree().create_timer(3.0).timeout
+		var scene_transition_screen_instance = scene_transition_screen.instantiate()
+		get_tree().get_root().add_child(scene_transition_screen_instance)
+		await get_tree().create_timer(5.0).timeout
 		get_tree().change_scene_to_file(scene_path)
+		scene_transition_screen_instance.queue_free()
